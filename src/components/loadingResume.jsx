d3105ccbab70e6c2ab20193fb0432aac38c6efd3
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-07-20 13:15:26 +0530

    commited

---
+++ src/components/loadingResume.jsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from "react";
+import CloudPdfViewer from "@cloudpdf/viewer";
+import { MdDownloadForOffline } from "react-icons/md"
+import {Link} from "react-router-dom"
+
+
+function loadingResume() {
+
+    
+  const viewer = useRef(null);
+  useEffect(() => {
+    CloudPdfViewer(
+      {
+        documentId: "b0943986-f729-4e25-bc21-8706f18c24af",
+        darkMode: true,
+        disableToolbar: true,
+        appBarColored: true,
+      },
+      viewer.current
+    ).then((instance) => {});
+  }, []);
+
+
+  return (
+    <>
+    <div className="w-full h-screen  bg-[#303030] absolute top-0  ">
+
+      <div className="viewer w-full h-full mt-20 p-0" ref={viewer}></div>
+
+
+      <Link to="https://cloudpdf.io/download/cAT1kTG3M" >
+      <div className="flex justify-center items-center fixed right-14 bottom-10 text-5xl  text-[#FFFDD0] max-md:text-[#774069] max-md:bottom-20 hover:text-[#774069]  duration-200 bg-transparent">
+        <MdDownloadForOffline className="hover:drop-shadow-c-purple" />
+      </div>
+      </Link>
+
+    </div>
+    </>
+  );
+}
+
+export default loadingResume
+
+
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,24 +1,25 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, Suspense, lazy } from "react";
 import CloudPdfViewer from "@cloudpdf/viewer";
-import Nav from '../components/Nav.jsx'
-import Social from "../components/Social.jsx";
 import { MdDownloadForOffline } from "react-icons/md"
 import {Link} from "react-router-dom"
+const LazyLoading = lazy( () => import('../components/loadingResume.jsx') )
 
 
 export default function Resume() {
-  const viewer = useRef(null);
-  useEffect(() => {
-    CloudPdfViewer(
-      {
-        documentId: "b0943986-f729-4e25-bc21-8706f18c24af",
-        darkMode: true,
-        disableToolbar: true,
-        appBarColored: true,
-      },
-      viewer.current
-    ).then((instance) => {});
-  }, []);
+  // const viewer = useRef(null);
+  // useEffect(() => {
+  //   CloudPdfViewer(
+  //     {
+  //       documentId: "b0943986-f729-4e25-bc21-8706f18c24af",
+  //       darkMode: true,
+  //       disableToolbar: true,
+  //       appBarColored: true,
+  //     },
+  //     viewer.current
+  //   ).then((instance) => {});
+  // }, []);
+
+  
   return (
     <>
       <div className="w-full h-screen  bg-[#303030] absolute top-0  ">
@@ -30,15 +31,18 @@ export default function Resume() {
         <h1 className='text-8xl text-[#E8251C]'>/</h1>
         <h1 className='text-6xl text-[#C07ABE]'>{" >"}</h1>
       </h1>
-      <div className="viewer w-full h-full mt-2 p-0" ref={viewer}></div>
 
       {/* <div className="viewer w-full h-full mt-20 p-0" ref={viewer}></div> */}
 
-      <Link to="https://cloudpdf.io/download/cAT1kTG3M" >
+      {/* <Link to="https://cloudpdf.io/download/cAT1kTG3M" >
       <div className="flex justify-center items-center fixed right-14 bottom-10 text-5xl  text-[#FFFDD0] max-md:text-[#774069] max-md:bottom-20 hover:text-[#774069]  duration-200 bg-transparent">
         <MdDownloadForOffline className="hover:drop-shadow-c-purple" />
       </div>
-      </Link>
+      </Link> */}
+
+        <Suspense fallback={<div>Loading...</div>}>
+        <LazyLoading />
+        </Suspense>
 
 
       </div>
